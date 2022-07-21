20980ca8253fea83d07157a026cff4f153bd9a7e
yacine boukhari <yacineboukhari@hotmail.com>
2022-07-21 11:41:57 +0200

    modifications site

---
+++ .history/serveur_20220713184555.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+app.use(express.static('folder containing your static files'));
+
+app.listen(port);
+console.log('Server started at http://localhost/:' + port);
